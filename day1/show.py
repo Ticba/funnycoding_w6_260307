072b93beb34f710cb8c6c834087ab1fe28848572
Ticba <ticba@ticba.me>
2026-03-07 20:10:35 +0800

    day1

---
+++ day1/show.py
@@ -0,0 +1,48 @@
+# print(12)
+# print(12+34)    # 12 + 34 = 46
+# print(12-34)    # 12 - 34 = -22
+# print(12*34)    # 12 * 34 = 408
+# print()         # /（算小數除法）, // （算整數的除法，取商）, % （算整數的除法，取餘）
+# print(12/34)
+# print(12//34)
+# print(12%34)
+# print(2**3)
+# 註解 (ctrl + /)
+# print(2+3*5-10)
+
+# print('12')     # '單引號夾住'、"雙引號夾住" (O)   |   '裡面的東西" (X)
+# print('12+34')  # '字串'
+
+
+
+
+
+# Variable 變數 -> box
+'''
+a = 12
+print(a)
+
+b = 12+34
+print(b)
+
+c = '12+34'
+print(c)
+'''
+
+
+# 選取 -> alt + up/down
+
+
+# print('soasdjkfl;asldkf')   # end = '\n' -> 換行
+
+a = 12
+print(a, end='***')     # end 箱子原本是裝 '\n'， 現在賦值成 '***'
+
+b = 12+34
+print(b, end='***')
+
+c = '12+34'
+print(c, end='***')
+
+
+# '=' 在 python 中代表「賦值」assign。 右邊賦值給左邊
