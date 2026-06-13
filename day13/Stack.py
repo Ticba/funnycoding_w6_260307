77b1147b1f58ebe913507b87ead423ba1e8dc850
Ticba <ticba@ticba.me>
2026-06-13 20:58:15 +0800

    day13

---
+++ day13/Stack.py
@@ -0,0 +1,29 @@
+class Stack:
+     def __init__(self):
+         self.items = []        # list
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[-1]
+
+     def size(self):
+         return len(self.items)
+     
+
+s = Stack()     # init()
+s.push(3)
+s.push(5)
+s.push(1)
+
+print(s.pop())
+print(s.pop())
+print(s.pop())
+print(s.isEmpty())
---
+++ day13/hw.md
@@ -0,0 +1,8 @@
+# 1. https://runestone.academy/ns/books/published/pythonds/AlgorithmAnalysis/AnAnagramDetectionExample.html
+3.4 的三個問題
+
+# 2. https://runestone.academy/ns/books/published/pythonds/BasicDS/SimpleBalancedParentheses.html
+4.6 讀完並理解（包含程式測試）
+
+# 3. https://runestone.academy/ns/books/published/pythonds/BasicDS/WhatIsaQueue.html
+4.10 讀完並理解
